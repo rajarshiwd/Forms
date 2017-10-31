09b8098bae58f5d34311e68dfc612b00baa8da35
Your Name <you@example.com>
2017-10-31 10:16:40 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import {RegistrationComponent} from './Registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
