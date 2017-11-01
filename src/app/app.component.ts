48284bef8476c6c07d842c5d274b3368c1e3cf44
rajarshi8055 <rajarshichowdary@gmail.com>
2017-11-01 07:11:19 -0500

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,44 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   headMessage : string;
   selectedRow : any;
-  setClickedRow : Function;
+
   filterArray:any = [];
   Domains:Array<String>;
   ParentDomains:Array<String>;
   ChildDomains:Array<String>;
   constructor(){
-      this.headMessage = "Please click below rows !!";
-      this.Domains = ["chemical","oil","gas","Power","Financial"];
-      this.ParentDomains = ["chemical","oil","gas","Power","Financial"];
+     
+    this.Domains = ["chemical","oil","gas","Power","Financial"];
+    this.ParentDomains = ["chemical","oil","gas","Power","Financial"];      
+      }
+      
+   
+      
  
 
-      this.setClickedRow = function(index){
-          this.selectedRow = this.ParentDomains[index];
-  
-          this.Domains = this.ParentDomains.filter(function(item) { 
-
-           
-            
-        
-         alert(this.selectedRow);
+      setClickedRow(domain){
           
-               
-                
+       
+          this.Domains = this.Domains.filter(function(item) { 
+
+              return item !== domain;
             })
             
            
-    }
-          
-      }
-      
-
-     
-
-
-  };
+    
+          }
+        }
 
