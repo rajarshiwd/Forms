0c4152b13d22ed3d76c85db3f94e62cd46f44cf2
rajarshi8055 <rajarshichowdary@gmail.com>
2017-10-31 22:15:06 -0500

    'hi'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+  headMessage : string;
+  selectedRow : any;
+  setClickedRow : Function;
+  filterArray:any = [];
+  Domains:Array<String>;
+  ParentDomains:Array<String>;
+  ChildDomains:Array<String>;
+  constructor(){
+      this.headMessage = "Please click below rows !!";
+      this.Domains = ["chemical","oil","gas","Power","Financial"];
+      this.ParentDomains = ["chemical","oil","gas","Power","Financial"];
+ 
+
+      this.setClickedRow = function(index){
+          this.selectedRow = this.ParentDomains[index];
+  
+          this.Domains = this.ParentDomains.filter(function(item) { 
+
+           
+            
+        
+         alert(this.selectedRow);
+          
+               
+                
+            })
+            
+           
+    }
+          
+      }
+      
+
+     
+
+
+  };
+
